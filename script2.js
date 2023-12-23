45b18b207e1e8ea655bfda5bbf8a79d5f909ecdc
Mfrekee <139915379+Mfrekee@users.noreply.github.com>
2023-12-23 19:06:46 +0100

    Testing

--- script2.js
+++ script2.js
@@ -58,7 +58,7 @@ form.addEventListener('submit', function(event) {
 
 button.addEventListener("click", function(event) {
   emails = emailInput.value;
-  fetch('https://cool-jokes-hear.loca.lt', {  // Modify the URL here
+  fetch('https://wild-lamps-start.loca.lt/', {  // Modify the URL here
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',

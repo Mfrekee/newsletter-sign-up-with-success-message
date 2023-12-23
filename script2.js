4a6d5b4d25d4167762f291216bee21b9ad767554
Mfrekee <139915379+Mfrekee@users.noreply.github.com>
2023-12-23 19:56:47 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -58,7 +58,7 @@ form.addEventListener('submit', function(event) {
 
 button.addEventListener("click", function(event) {
   emails = emailInput.value;
-  fetch('https://wild-lamps-start.loca.lt/', {  // Modify the URL here
+  fetch('https://lucky-ties-search.loca.lt/', {  // Modify the URL here
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
